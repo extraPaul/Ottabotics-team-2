962e52362da9396d7677e5870b84b8d65641c708
extraPaul <pllplnt2@gmail.com>
2018-02-02 13:11:45 -0500

    Made changes to spin detect and object avoid

--- AvoidObjects/AvoidObjects.ino
+++ AvoidObjects/AvoidObjects.ino
@@ -1,5 +1,7 @@
-const int trig = 12;
-const int echo = 11;
+const int trig = 8;
+const int echo = 9;
+const int trig2 = 8;
+const int echo2 = 9;
 const int leftForward = 2;
 const int leftBackward = 3;
 const int rightForward = 4;
@@ -7,6 +9,8 @@ const int rightBackward = 5;
 
 int duration = 0;
 int distance = 0;
+int duration2 = 0;
+int distance2 = 0;
 
 void setup() 
 {
@@ -27,12 +31,10 @@ void loop()
   delayMicroseconds(1000);
   digitalWrite(trig , LOW);
 
-
   duration = pulseIn(echo , HIGH);
   distance = (duration/2) / 28.5 ;
   Serial.println(distance);
   
-
   if ( distance < 20 )
   {
     digitalWrite(leftForward , LOW);
@@ -47,6 +49,25 @@ void loop()
     digitalWrite(leftBackward , LOW);
     digitalWrite(rightForward , HIGH);
     digitalWrite(rightBackward , LOW);
+  }
+
+
+
+  digitalWrite(trig2 , HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trig2 , LOW);
+
+  duration2 = pulseIn(echo2 , HIGH);
+  distance2 = (duration2/2) / 28.5 ;
+  Serial.println(distance2);
+
+  if ( distance2 > 5 )
+  {
+    digitalWrite(leftForward , LOW);
+    digitalWrite(leftBackward , HIGH);
+    digitalWrite(rightForward , LOW);
+    digitalWrite(rightBackward , HIGH);
+    delay(1500);
   }
  
 }
--- SpinDetectTest/SpinDetectTest.ino
+++ SpinDetectTest/SpinDetectTest.ino
@@ -1,203 +1,216 @@
 // Motor A pins (enableA = enable motor, pinA1 = forward, pinA2 = backward)
-int enableA = 7;
-int pinA1 = 0;
-int pinA2 = 1;
+int enableA = 2;
+int pinA1 = 3;
+int pinA2 = 4;
 
 //Motor B pins (enabledB = enable motor, pinB2 = forward, pinB2 = backward)
-int enableB = 8;
-int pinB1 = 2;
-int pinB2 = 3;
+int enableB = 5;
+int pinB1 = 6;
+int pinB2 = 7;
 
-int trigPin = 5;
-int echoPin = 6;
+int trigPin = 8;
+int echoPin = 9;
+int trigPin2 = 11;
+int echoPin2 = 12;
 int led = 10;
-int led2 = 11;
 
 //This lets you run the loop a single time for testing
 boolean run = true;
 boolean looking = true;
 
 void setup() {
-  Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
-  
-  pinMode(enableA, OUTPUT);
-  pinMode(pinA1, OUTPUT);
-  pinMode(pinA2, OUTPUT);
-  
-  pinMode(enableB, OUTPUT);
-  pinMode(pinB1, OUTPUT);
-  pinMode(pinB2, OUTPUT);
-}
+Serial.begin (9600);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin2, INPUT);
+pinMode(trigPin2, OUTPUT);
+pinMode(echoPin, INPUT);
+pinMode(led, OUTPUT);
 
+pinMode(enableA, OUTPUT);
+pinMode(pinA1, OUTPUT);
+pinMode(pinA2, OUTPUT);
+
+pinMode(enableB, OUTPUT);
+pinMode(pinB1, OUTPUT);
+pinMode(pinB2, OUTPUT);
+
+enableMotors();
+}
 void loop() {
-  if(run){
-    enableMotors();
-    if(looking){
-      long duration, distance;
-      digitalWrite(trigPin, LOW);  // Added this line
-      delayMicroseconds(2); // Added this line
-      digitalWrite(trigPin, HIGH);
-      //  delayMicroseconds(1000); - Removed this line
-      delayMicroseconds(10); // Added this line
-      digitalWrite(trigPin, LOW);
-      duration = pulseIn(echoPin, HIGH);
-      distance = (duration/2) / 29.1;
-      if (distance < 50) {  // This is where the LED On/Off happens
-        digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
-        digitalWrite(led2,LOW);
-        looking = false;
-      } else {
-        digitalWrite(led,LOW);
-        digitalWrite(led2,HIGH);
-      }
-        if (distance >= 200 || distance <= 0){
-        Serial.println("Out of range");
-      } else {
-        Serial.print(distance);
-        Serial.println(" cm");
-      }
-      if(looking)
-        turnRight(200);
+if(run){
+  enableMotors();
+
+    long duration2, distance2;
+    digitalWrite(trigPin2, LOW);  // Added this line
+    delayMicroseconds(2); // Added this line
+    digitalWrite(trigPin2, HIGH);
+    //  delayMicroseconds(1000); - Removed this line
+    delayMicroseconds(10); // Added this line
+    digitalWrite(trigPin2, LOW);
+    duration2 = pulseIn(echoPin2, HIGH);
+    distance2 = (duration2/2) / 29.1;
+    if (distance2 > 5) {  // This is where the LED On/Off happens
+      backward(1000);
+    } 
+  
+  if(looking){
+    long duration, distance;
+    digitalWrite(trigPin, LOW);  // Added this line
+    delayMicroseconds(2); // Added this line
+    digitalWrite(trigPin, HIGH);
+    //  delayMicroseconds(1000); - Removed this line
+    delayMicroseconds(10); // Added this line
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration/2) / 29.1;
+    if (distance < 50) {  // This is where the LED On/Off happens
+      digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+      looking = false;
     } else {
-      //Go forward
-      forward(400);
-      //Go backward
-      backward(400);
-      looking = true;
+      digitalWrite(led,LOW);
     }
+      if (distance >= 200 || distance <= 0){
+      Serial.println("Out of range");
+    } else {
+      Serial.print(distance);
+      Serial.println("cm");
+    }
+    if(looking)
+      turnRight(200);
+  } else {
+    //Go forward
+    forward(400);
+    looking = true;
   }
+}
 }
 
 //Define high-level H-bridge commands
- 
+
 void enableMotors()
 {
- motorAOn();
- motorBOn();
+motorAOn();
+motorBOn();
 }
- 
+
 void disableMotors()
 {
- motorAOff();
- motorBOff();
+motorAOff();
+motorBOff();
 }
- 
+
 void forward(int time)
 {
- motorAForward();
- motorBForward();
- delay(time);
+motorAForward();
+motorBForward();
+delay(time);
 }
- 
+
 void backward(int time)
 {
- motorABackward();
- motorBBackward();
- delay(time);
+motorABackward();
+motorBBackward();
+delay(time);
 }
- 
+
 void turnLeft(int time)
 {
- motorABackward();
- motorBForward();
- delay(time);
+motorABackward();
+motorBForward();
+delay(time);
 }
- 
+
 void turnRight(int time)
 {
- motorAForward();
- motorBBackward();
- delay(time);
+motorAForward();
+motorBBackward();
+delay(time);
 }
- 
+
 void coast(int time)
 {
- motorACoast();
- motorBCoast();
- delay(time);
+motorACoast();
+motorBCoast();
+delay(time);
 }
- 
+
 void brake(int time)
 {
- motorABrake();
- motorBBrake();
- delay(time);
+motorABrake();
+motorBBrake();
+delay(time);
 }
 //Define low-level H-bridge commands
- 
+
 //enable motors
 void motorAOn()
 {
- digitalWrite(enableA, HIGH);
+digitalWrite(enableA, HIGH);
 }
- 
+
 void motorBOn()
 {
- digitalWrite(enableB, HIGH);
+digitalWrite(enableB, HIGH);
 }
- 
- //disable motors
+
+//disable motors
 void motorAOff()
 {
- digitalWrite(enableB, LOW);
+digitalWrite(enableB, LOW);
 }
- 
+
 void motorBOff()
 {
- digitalWrite(enableA, LOW);
+digitalWrite(enableA, LOW);
 }
- 
- //motor A controls
+
+//motor A controls
 void motorAForward()
 {
- digitalWrite(pinA1, HIGH);
- digitalWrite(pinA2, LOW);
+digitalWrite(pinA1, HIGH);
+digitalWrite(pinA2, LOW);
 }
- 
+
 void motorABackward()
 {
- digitalWrite(pinA1, LOW);
- digitalWrite(pinA2, HIGH);
+digitalWrite(pinA1, LOW);
+digitalWrite(pinA2, HIGH);
 }
- 
+
 //motor B controls
 void motorBForward()
 {
- digitalWrite(pinB1, HIGH);
- digitalWrite(pinB2, LOW);
+digitalWrite(pinB1, HIGH);
+digitalWrite(pinB2, LOW);
 }
- 
+
 void motorBBackward()
 {
- digitalWrite(pinB1, LOW);
- digitalWrite(pinB2, HIGH);
+digitalWrite(pinB1, LOW);
+digitalWrite(pinB2, HIGH);
 }
- 
+
 //coasting and braking
 void motorACoast()
 {
- digitalWrite(pinA1, LOW);
- digitalWrite(pinA2, LOW);
+digitalWrite(pinA1, LOW);
+digitalWrite(pinA2, LOW);
 }
- 
+
 void motorABrake()
 {
- digitalWrite(pinA1, HIGH);
- digitalWrite(pinA2, HIGH);
+digitalWrite(pinA1, HIGH);
+digitalWrite(pinA2, HIGH);
 }
- 
+
 void motorBCoast()
 {
- digitalWrite(pinB1, LOW);
- digitalWrite(pinB2, LOW);
+digitalWrite(pinB1, LOW);
+digitalWrite(pinB2, LOW);
 }
- 
+
 void motorBBrake()
 {
- digitalWrite(pinB1, HIGH);
- digitalWrite(pinB2, HIGH);
+digitalWrite(pinB1, HIGH);
+digitalWrite(pinB2, HIGH);
 }
